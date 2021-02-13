b7e5ede9890cb56b61783f263daaaa36688e4add
Soichiro Ota <libarta.official@gmail.com>
2021-02-13 16:29:18 +0900

    cargo fmt

--- dstump/src/lib.rs
+++ dstump/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate ndarray;
-use ndarray::{Array, Ix1, Ix2};
 use ndarray::prelude::*;
+use ndarray::{Array, Ix1, Ix2};
 
-use rsklearn::entropy::{gini};
-use rsklearn_zeror::{ZeroRule};
-use rsklearn_linear::{Linear};
+use rsklearn::entropy::gini;
+use rsklearn_linear::Linear;
+use rsklearn_zeror::ZeroRule;
 
 pub struct DecisionStump {
     metric: fn(Array<f64, Ix2>) -> f64,
@@ -34,7 +34,7 @@ impl DecisionStump {
         }
     }
 
-    pub fn make_split(&self, feat: Array::<f64, Ix1>, val: f64) -> (Vec<usize>, Vec<usize>) {
+    pub fn make_split(&self, feat: Array<f64, Ix1>, val: f64) -> (Vec<usize>, Vec<usize>) {
         let mut left = Vec::new();
         let mut right = Vec::new();
         for (i, v) in feat.iter().enumerate() {
@@ -47,9 +47,9 @@ impl DecisionStump {
         (left, right)
     }
 
-    pub fn make_loss(&self, y1: Array::<f64, Ix2>, y2: Array::<f64, Ix2>) -> f64 {
+    pub fn make_loss(&self, y1: Array<f64, Ix2>, y2: Array<f64, Ix2>) -> f64 {
         if y1.shape()[0] == 0 || y2.shape()[0] == 0 {
-            return f64::INFINITY
+            return f64::INFINITY;
         }
         let total = y1.shape()[0] as f64 + y2.shape()[0] as f64;
         let metric = self.metric;
@@ -58,7 +58,11 @@ impl DecisionStump {
         m1 + m2
     }
 
-    pub fn split_tree(&mut self, x: Array::<f64, Ix2>, y: Array::<f64, Ix2>) -> (Vec<usize>, Vec<usize>) {
+    pub fn split_tree(
+        &mut self,
+        x: Array<f64, Ix2>,
+        y: Array<f64, Ix2>,
+    ) -> (Vec<usize>, Vec<usize>) {
         self.feat_index = 0;
         self.feat_val = f64::INFINITY;
         let mut score = f64::INFINITY;
@@ -70,11 +74,13 @@ impl DecisionStump {
                 let (l, r) = self.make_split(feat.to_owned(), *val);
                 let mut y_l = Array::<f64, Ix2>::zeros((y.shape()[0], l.len()));
                 for l_elm in l.iter() {
-                    y_l.slice_mut(s![*l_elm, ..]).assign(&y.slice(s![*l_elm, ..]))
+                    y_l.slice_mut(s![*l_elm, ..])
+                        .assign(&y.slice(s![*l_elm, ..]))
                 }
                 let mut y_r = Array::<f64, Ix2>::zeros((y.shape()[0], r.len()));
                 for r_elm in r.iter() {
-                    y_r.slice_mut(s![*r_elm, ..]).assign(&y.slice(s![*r_elm, ..]))
+                    y_r.slice_mut(s![*r_elm, ..])
+                        .assign(&y.slice(s![*r_elm, ..]))
                 }
                 let loss = self.make_loss(y_l, y_r);
                 if score > loss {
