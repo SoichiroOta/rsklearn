63b930f6dfb2a527f1448951ccd11c7b7bd0894e
Soichiro Ota <libarta.official@gmail.com>
2021-02-13 14:50:00 +0900

    cargo fmt

--- linear/src/lib.rs
+++ linear/src/lib.rs
@@ -1,5 +1,5 @@
 use ndarray::prelude::*;
-use ndarray::{Array, Ix1, Ix2, Axis};
+use ndarray::{Array, Axis, Ix1, Ix2};
 use ndarray_stats::QuantileExt;
 
 pub struct Linear {
@@ -7,7 +7,7 @@ pub struct Linear {
     lr: f64,
     early_stop: Option<f64>,
     beta: Option<f64>,
-    norm: Option<Array::<f64, Ix2>>,
+    norm: Option<Array<f64, Ix2>>,
 }
 
 /// Configure and fit a linear regression model
@@ -15,43 +15,41 @@ impl Linear {
     /// Create a default linear regression model.
     pub fn new(epochs: usize, lr: f64, early_stop: Option<f64>) -> Linear {
         Linear {
-             epochs,
-             lr,
-             early_stop,
-             beta: None,
-             norm: None,
+            epochs,
+            lr,
+            early_stop,
+            beta: None,
+            norm: None,
         }
     }
 
-    pub fn fit_norm(mut self, x: Array::<f64, Ix2>, y: Array::<f64, Ix1>) -> Self {
+    pub fn fit_norm(mut self, x: Array<f64, Ix2>, y: Array<f64, Ix1>) -> Self {
         let mut norm = Array::<f64, Ix2>::zeros((x.shape()[1] + 1, 2));
 
         norm.slice_mut(s![0, 0]).fill(*y.min().unwrap());
         norm.slice_mut(s![0, 1]).fill(*y.max().unwrap());
 
-        let x_min = x.map_axis(Axis(0), |view| view.iter().fold(0.0/0.0, |m, v| v.min(m)));
+        let x_min = x.map_axis(Axis(0), |view| view.iter().fold(0.0 / 0.0, |m, v| v.min(m)));
         norm.slice_mut(s![1.., 0]).assign(&x_min);
-        let x_max = x.map_axis(Axis(0), |view| view.iter().fold(0.0/0.0, |m, v| v.max(m)));
+        let x_max = x.map_axis(Axis(0), |view| view.iter().fold(0.0 / 0.0, |m, v| v.max(m)));
         norm.slice_mut(s![1.., 1]).assign(&x_max);
 
         self.norm = Some(norm);
         self
     }
 
-    pub fn normalize(self, x: Array::<f64, Ix2>, y: Option<Array::<f64, Ix1>>) -> (Array::<f64, Ix2>, Option<Array::<f64, Ix1>>) {
+    pub fn normalize(
+        self,
+        x: Array<f64, Ix2>,
+        y: Option<Array<f64, Ix1>>,
+    ) -> (Array<f64, Ix2>, Option<Array<f64, Ix1>>) {
         let norm = self.norm.unwrap();
         let mut l = &norm.slice(s![1.., 1]) - &norm.slice(s![1.., 0]);
-        l = l.mapv(|v: f64| if v == 0.0 {
-            1.0
-        } else {
-            v
-        });
+        l = l.mapv(|v: f64| if v == 0.0 { 1.0 } else { v });
         let mut p = &x - &norm.slice(s![1.., 0]);
         p = &p / &l;
         let p_q = match y {
-            None => {
-                (p, y)
-            },
+            None => (p, y),
             Some(y) => {
                 let mut q = y;
                 if !(norm[[0, 1]] == norm[[0, 0]]) {
@@ -64,7 +62,7 @@ impl Linear {
         p_q
     }
 
-    pub fn r2(self, y: Array::<f64, Ix1>, z: Array::<f64, Ix1>) -> f64 {
+    pub fn r2(self, y: Array<f64, Ix1>, z: Array<f64, Ix1>) -> f64 {
         let mut y_minus_z_pow2 = &y - &z;
         y_minus_z_pow2 = y_minus_z_pow2.mapv(|v: f64| v.powi(2));
         let mn = y_minus_z_pow2.sum();
@@ -87,7 +85,7 @@ impl Default for Linear {
         Linear {
             epochs: 20,
             lr: 0.01,
-            early_stop:None,
+            early_stop: None,
             beta: None,
             norm: None,
         }
